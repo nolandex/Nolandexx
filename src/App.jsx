3d572f5227dd4cc1dcc39d3d31a4d97a3ca2d170
nolandex <stemapal1234@gmail.com>
2025-05-03 11:45:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,7 +12,6 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
