a7e7346e03433a3a6b629d3f6e520e10fd10b6b9
nolandex <stemapal1234@gmail.com>
2025-05-03 15:20:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Services from "./pages/Services";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,9 +9,7 @@ function App() {
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
       <Footer />
