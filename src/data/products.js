b582278652e85cc12255dc008e3a69f5ec7d26c9
nolandex <stemapal1234@gmail.com>
2025-05-04 14:18:34 +0700

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,57 @@
+export const products = [
+  {
+    id: 1,
+    name: "Kopi",
+    variants: [
+      { id: 1, name: "Espresso", price: 15000 },
+      { id: 2, name: "Cappuccino", price: 20000 },
+      { id: 3, name: "Latte", price: 22000 },
+      { id: 4, name: "Americano", price: 18000 },
+      { id: 5, name: "Mocha", price: 25000 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Teh",
+    variants: [
+      { id: 1, name: "Green Tea", price: 12000 },
+      { id: 2, name: "Black Tea", price: 10000 },
+      { id: 3, name: "Chamomile", price: 15000 },
+      { id: 4, name: "Earl Grey", price: 14000 },
+      { id: 5, name: "Matcha", price: 20000 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Makanan Ringan",
+    variants: [
+      { id: 1, name: "Kentang Goreng", price: 18000 },
+      { id: 2, name: "Onion Ring", price: 16000 },
+      { id: 3, name: "Nugget Ayam", price: 20000 },
+      { id: 4, name: "Tempe Goreng", price: 12000 },
+      { id: 5, name: "Tahu Isi", price: 14000 },
+    ],
+  },
+  {
+    id: 4,
+    name: "Roti",
+    variants: [
+      { id: 1, name: "Croissant", price: 15000 },
+      { id: 2, name: "Baguette", price: 20000 },
+      { id: 3, name: "Donat", price: 10000 },
+      { id: 4, name: "Roti Cokelat", price: 12000 },
+      { id: 5, name: "Roti Keju", price: 18000 },
+    ],
+  },
+  {
+    id: 5,
+    name: "Jus Buah",
+    variants: [
+      { id: 1, name: "Jus Jeruk", price: 15000 },
+      { id: 2, name: "Jus Mangga", price: 18000 },
+      { id: 3, name: "Jus Apel", price: 16000 },
+      { id: 4, name: "Jus Alpukat", price: 20000 },
+      { id: 5, name: "Jus Stroberi", price: 17000 },
+    ],
+  },
+];
